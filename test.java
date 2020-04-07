948992b0b5af72cb6305fed8248fed768ab29524
ilariagallo <ilaria.gallo@echobox.com>
2020-04-07 12:45:06 +0200

    Commit on the new branch

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class test{
 
     public static void main(String[] args) {
         System.out.println("Welcome to Git");
+        System.out.println("New branch modify test.java");
     }
 }
 
